4f65d2090c9fd52d260c3836da9bfd9318c42147
Brandon R. Stoner <monokrome@monokro.me>
2010-10-28 19:57:54 -0600

    Added some generic templates to the timer application.

--- src/timesheet/timer/views.py
+++ src/timesheet/timer/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from timesheet.timer.models import Activity
+from django.template import RequestContext
+
+def activity_timer(request):
+    template_data = {}
+    return render_to_response('timer/activity_timer.html', template_data,
+               context_instance=RequestContext(request))
+
