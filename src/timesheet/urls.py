84c79e035fb616ad8cbfd19fdea28802ca89a770
Brandon R. Stoner <monokrome@monokro.me>
2010-10-28 19:29:11 -0600

    Added admin to URLs.

--- src/timesheet/urls.py
+++ src/timesheet/urls.py
@@ -4,12 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^timesheet/', include('timesheet.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
