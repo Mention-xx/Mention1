f3dfcfb466863fc63e06fb942e2c4faff5fee2ad
Brandon R. Stoner <monokrome@monokro.me>
2010-10-30 21:12:48 -0600

    Added the actual clocking functionality.

--- src/timesheet/timer/views.py
+++ src/timesheet/timer/views.py
@@ -12,13 +12,31 @@ import datetime
 def activity_timer(request, *args, **kwargs):
     current_time = datetime.datetime.now()
 
+    if request.method == 'POST':
+        try:
+            this_activity = Activity.objects.get(delegate=request.user, finished_original=None)
+            this_activity.finished_original = datetime.datetime.now()
+
+        except Activity.DoesNotExist:
+            this_activity = Activity()
+            this_activity.started_original = datetime.datetime.now()
+
+        if this_activity.delegate is None:
+            this_activity.delegate = request.user
+
+        this_activity.save()
+
+        # Delete activities that don't consist of at least 15 minutes. TODO: Verify that we want this.
+        if this_activity.started == this_activity.finished:
+            this_activity.delete()
+
+        return HttpResponseRedirect('/')
+
     template_data = {
         'current_time': current_time.strftime('%A, %B ') + ordinal(current_time.strftime('%d')) + current_time.strftime(' %I:%M:%S %p'),
-        'todays_activities': Activity.objects.today().filter(delegate = request.user)
+        'todays_activities': Activity.objects.today().filter(delegate = request.user),
+        'clock_in': clock_in
     }
-
-    if request.method == 'POST':
-        return HttpResponseRedirect('/')
 
     return render_to_response('timer/activity_timer.html', template_data,
                context_instance=RequestContext(request))
