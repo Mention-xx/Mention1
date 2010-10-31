8e8095200825d3c94d5ce1a29bec4cb04f19cd45
Brandon R. Stoner <monokrome@monokro.me>
2010-10-30 20:12:51 -0600

    Modified timer to use a method property for start and finish time, so that rounding to the nearest 15 minutes is possible without polluting the original data.

--- src/timesheet/timer/models.py
+++ src/timesheet/timer/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 ### TODO: Apply i18n translations to this text
 ACTIVITY_CHOICES = (
@@ -19,14 +20,21 @@ ACTIVITY_CHOICES = (
     (9000, 'Additional Cost'),
 )
 
+def round_minutes_to_nearest_fifteen(original_datetime):
+    return original_datetime + datetime.timedelta(**{
+               'minutes': (round(original_datetime.minute/15)*15) - original_datetime.minute,
+               'seconds': original_datetime.second * -1,
+               'microseconds': original_datetime.microsecond * -1,
+           })
+
 class Activity(models.Model):
     """ A piece of work that has been done within a specific amount of time. """
 
     class Meta(object):
         verbose_name_plural = 'activities'
 
-    started = models.DateTimeField()
-    finished = models.DateTimeField(null=True, blank=True)
+    started_original = models.DateTimeField("started")
+    finished_original = models.DateTimeField("finished", null=True, blank=True)
 
     delegate = models.ForeignKey(User, null=True, blank=True)
 
@@ -37,6 +45,22 @@ class Activity(models.Model):
     )
 
     description = models.TextField(null=True, blank=True)
+
+    @property
+    def started(self):
+        return round_minutes_to_nearest_fifteen(self.started_original)
+
+    @started.setter
+    def started(self, value):
+        self.started_original = value
+
+    @property
+    def finished(self):
+        return round_minutes_to_nearest_fifteen(self.finished_original)
+
+    @finished.setter
+    def finished(self, value):
+        self.started_original = value
 
     def time_taken(self):
         """ Get the amount of time that this project has taken. """
