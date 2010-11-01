8b2fbd3e2e8f00234926378a604ca3a623f785d5
Brandon R. Stoner <monokrome@monokro.me>
2010-10-31 19:57:15 -0600

    Modified URLs for new timesheet stuff.

--- src/timesheet/urls.py
+++ src/timesheet/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -7,7 +8,8 @@ urlpatterns = patterns('',
     (r'^account/', include('django.contrib.auth.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^timesheet/', include('timesheet.timer.urls')),
 
-    (r'^$', 'timesheet.timer.views.activity_timer'),
+    (r'^$', 'timesheet-activity-list'),
 )
 
