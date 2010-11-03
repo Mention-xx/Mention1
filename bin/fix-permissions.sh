7970d262d55ecd89f9c7b7cc66b86e59fa887b95
timesheet <timesheet@joy.servers.monokro.me>
2010-11-03 02:51:02 +0000

    Updated permission script for EBS volume.

--- bin/fix-permissions.sh
+++ bin/fix-permissions.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 chown -R timesheet:timesheet /home/timesheet
-chmod -R 770 /home/timesheet
+chmod -R 770                 /home/timesheet
+chown -R timesheet:timesheet /mnt/static/webapps//timesheet
+chmod -R 770                 /mnt/static/webapps//timesheet
 
 find /home/timesheet -type d -exec chmod g+s {} \;
+find /mnt/static/webapps/timesheet -type d -exec chmod g+s {} \;
 
